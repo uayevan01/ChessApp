45328e92329dbb320e3d2ad92903884dd384f4d1
uayevan01 <uayevan01@gmail.com>
2022-04-20 17:35:46 -0400

    gitignore

--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stockfish = require('stockfish');
-
-router.get('/stockfish/move', (req, res, next) => {
-    res.json(stockfish)
-})
-
-module.exports = router;
